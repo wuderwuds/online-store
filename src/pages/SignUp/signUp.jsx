a36c722720536b5a0b8a925edab4313422b66398
barbalusa <antonkravchenkooutlook.com>
2024-04-03 11:38:15 +0300

    good forms

--- src/pages/SignUp/signUp.jsx
+++ src/pages/SignUp/signUp.jsx
@@ -1,71 +1,75 @@
-import { Formik, Field, Form } from 'formik';
 import * as Yup from 'yup';
 import {  useNavigate } from 'react-router-dom'
 import styles from './signUp.module.css'
 import { useMutation } from '@tanstack/react-query';
+import { FormRegAuth } from '../../components/Forms/formRegAuth';
+import { useNoAuth } from '../../hooks/useNoAuth';
 
-const signInSchema = Yup.object().shape({
-  email: Yup.string()
-            .email('Некорректный email')
-            .required('Required'),
-  password: Yup.string()
-               .required('Required'),
-});
 
 export const SignUp = () => {
-const navigate = useNavigate()
+    
+    const {token} = useNoAuth()
+    
+    const navigate = useNavigate();
+    
+    const signInSchema = Yup.object().shape({
+        email: Yup.string()
+                  .email('Некорректный email')
+                  .required('Введите email'),
+        password: Yup.string()
+                     .matches(RegExp("(.*[a-z].*)"), "")
+                     .matches(RegExp("(.*[A-Z].*)"), "Как минимум одна заглавная буква")
+                     .matches(RegExp("(.*\\d.*)"), "Как минимум одна цифра")
+                     .matches(RegExp('[!@#$%^&*(),.?":{}|<>]'), "Как минимум один спец. символ")
+                     .min(6, 'длина пароля не менее 6')
+                     .max(20, 'длина пароля не более 20')              
+                     .required('Введите пароль'),
+        group: Yup.string()
+                  .oneOf(["group-11"], 'Введите group-11')
+                  .required('Обязательно к запалнению')
+    });
+  
+    const initialValues = {
+        email: '',
+        password: '',
+        group: '',
+    };
 
-const initialValues = {
-  email: '',
-  password: '',
-  group: '',
-}
+    const {mutateAsync} = useMutation({
+        mutationFn: async (values) => {
+            const res = await fetch('https://api.react-learning.ru/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(values)
+        }) 
+        
+        return res;
+        }
+    });
 
-const {mutateAsync} = useMutation({
-  mutationFn: async (values) => {
-  const res = await fetch('https://api.react-learning.ru/signup', {
-  method: 'POST',
-  headers: {
-  'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(values)
-  }) 
-  return res
-  }})
+    const onSubmit = async (values) => {
+        const res = await mutateAsync(values)
+        const responce = await res.json()
+        if (res.ok) {
+            return navigate('/signin')
+        } alert(responce.message)
+    }
 
-const onSubmit = async (values) => {
-const res = await mutateAsync(values)
-const responce = await res.json()
-console.log(responce);   
-if (res.ok) {
-      return navigate('/signin')
-} alert(responce.message)
-}
-
-return (
-
-<div className={styles.wrapper1}>
-<h1>Sign Up</h1>
-<Formik
-initialValues={initialValues}
-onSubmit={onSubmit}
-validationSchema={signInSchema}
->
-<Form className={styles.wrapper}>
-<label htmlFor="email">Email</label>
-<Field
-id="email"
-name="email"
-placeholder="jane@acme.com"
-type="email"
-/>
-<label htmlFor="password">Password</label>
-<Field id="password" name="password" placeholder="password" type='password' />
-<label htmlFor="passgroupword">Группа </label>
-<Field id="group" name="group" placeholder="group-11" type='text'/>
-<button className="btn btn-success mt-1" type="submit">Зарегистрироваться</button>
-</Form>
-</Formik>
-</div>
-)
+    return (
+        <>
+        {!token &&
+        <div className={styles.wrapper}>
+            <h1>Sign Up</h1>
+                <FormRegAuth
+                typeForm={'reg'} 
+                validationSchema={signInSchema} 
+                onSubmit={onSubmit}
+                initialValues={initialValues}
+            />
+        </div>
+        }
+        </>
+    )
 }
